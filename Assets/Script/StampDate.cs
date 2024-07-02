aa8b83dccbdf83b2d336ce74af4a0d3253b98956
이지연 <a01092857503@gmail.com>
2024-07-02 13:41:38 +0900

    캐릭터 애니메이션 추가
    + 운동 애니메이션

---
+++ Assets/Script/StampDate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StampDate : MonoBehaviour
+{
+    [SerializeField] Text Text_TrainingStartDate;
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
